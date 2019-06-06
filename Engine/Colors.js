10e7902bc88f8d4460dff59d66f63738178dc1bb
Drew Banyai <DrewBanyai@gmail.com>
2019-06-05 20:46:40 -0700

    - Made an "Engine" folder for non-game related things and moved Colors.js and the ThreeJS code into it - Made "GameColors.js" which holds game-specific colors, and adds them to the Colors dictionary/map at program start

---
+++ Engine/Colors.js
@@ -0,0 +1,13 @@
+//  These colors are usable anywhere in the program
+var Colors = {
+	//  Basic colors to be used across the project
+	cyan: 					"rgb(36, 128, 121)",
+	brown: 					"rgb(169,143,120)",
+	brownDark: 				"rgb(154, 97, 105)",
+	green: 					"rgb(101, 187, 97)",
+	greenLight: 			'rgb(171, 214, 106)',
+	blue:					"rgb(107, 198, 255)",
+
+	//  This is generally returned if no color exists for a given object
+	Unknown:				"rgb(255, 0, 255)",
+};
---
+++ Program/GameData/GameColors.js
@@ -0,0 +1,14 @@
+//  Used by the Groundblock for basic block type colors
+Colors.groundblock_dirt = "rgb(89, 60, 31)";
+Colors.groundblock_grass = "rgb(171, 214, 106)";
+Colors.groundblock_water = "rgb(107, 198, 255)";
+
+//  Colors of various world objects
+Colors.TreeTrunk = "rgb(154, 97, 105)";
+Colors.TreeTop = "rgb(101, 187, 97)";
+Colors.GrassBlade = "rgb(40, 160, 0)";
+
+Colors.Beans_Seed = "rgb(210, 105, 30)";
+Colors.Beans_Sprout = "rgb(160, 165, 30)";
+Colors.Beans_Youth = "rgb(130, 195, 30)";
+Colors.Beans_Grown = "rgb(100, 215, 30)";
